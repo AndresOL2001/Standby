aff6aa47c6de18078c32bdbd25b3aba247fbce63
TheKevino <kevinespinoza00@outlook.com>
2023-07-08 14:49:15 -0700

    seleccion de accesos y su precio

--- lib/views/Accesos.dart
+++ lib/views/Accesos.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:standby/model/acceso.dart';
 
 import '../widgets/accesos_radio.dart';
 
 // ignore: must_be_immutable
 class Accesos extends StatelessWidget {
-  Accesos({super.key});
+  const Accesos({super.key});
 
   @override
   Widget build(BuildContext context) {
